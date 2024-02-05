653c7d81f6049a6f7b33bea0a2b0f9352df4a90a
tjhind <tjhind9@sky.com>
2024-02-05 12:28:08 +0000

    add changes to repo structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import HomepageManager from "../components/HomepageManager";
+import { Container } from "@mui/material";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Header />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomepageManager />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
