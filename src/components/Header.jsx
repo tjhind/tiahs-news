9fbd0ca5755cce9b8b6aebdf4d684495b0081add
tjhind <tjhind9@sky.com>
2024-02-07 15:42:32 +0000

    update styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
-import { Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Container, Box } from "@mui/material";
+
 export default function Header() {
   return (
-    <Typography variant="h6">
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <header className="header">
+    <Container id="header">
+      <Box className="header">
         <h1>Tiah's News</h1>
-      </header>
-    </Typography>
+      </Box>
+    </Container>
   );
 }
